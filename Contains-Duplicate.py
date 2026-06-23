0d942ed9b067fbfeaf5ac71d279d710136bf1257
Animesh Vijay Kewale <avk2473@gmail.com>
2026-06-23 14:28:58 +0530

    [217] [Time Beats: 31.87%] [Memory Beats: 10.11%] - LeetPush

---
+++ Contains-Duplicate.py
@@ -0,0 +1,9 @@
+1class Solution:
+2    def containsDuplicate(self, nums: List[int]) -> bool:
+3        d = {}
+4        for num in nums:
+5            if num in d:
+6                return True
+7            d[num] = True
+8
+9        return False
